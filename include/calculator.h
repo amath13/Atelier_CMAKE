842955dcc289e8b73e5fcf00277ba9f977c4940f
amath13 <166651762+amath13@users.noreply.github.com>
2024-05-23 20:25:48 +0200

    Update calculator.h

--- include/calculator.h
+++ include/calculator.h
@@ -1,10 +1,5 @@
-#ifndef CALCULATOR_H
-#define CALCULATOR_H
-
 double _add(double a, double b);
 double _sub(double a, double b);
 double _mul(double a, double b);
 double _div(double a, double b);
-double _carre(double a);
-
-#endif // CALCULATOR_H
+double _car(double a);
