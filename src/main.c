7d8694ec454497b884f3842fc22ef8180423f05f
amath13 <166651762+amath13@users.noreply.github.com>
2024-05-23 11:45:52 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,39 +3,37 @@
 #include <string.h>
 #include "./../include/calculator.h"
 
-int main(int argc, char *argv[]) {
-    if (argc == 4) {
-        char* op = argv[1];
-        char* a = argv[2];
-        char* b = argv[3];
-        double r = 0;
-        
-        if (strcmp(op, "add") == 0) {
-            r = _add(atof(a), atof(b));
-            printf("%lf", r);
-        }
-        else if (strcmp(op, "sub") == 0) {
-            r = _sub(atof(a), atof(b));
-            printf("%lf", r);
-        }
-        else if (strcmp(op, "mul") == 0) {
-            r = _mul(atof(a), atof(b));
-            printf("%lf", r);
-        }
-        else if (strcmp(op, "div") == 0) {
-            r = _div(atof(a), atof(b));
-            printf("%lf", r);
-        }
-        else if (strcmp(op, "square") == 0) {  // Ajout pour le calcul du carré
-            r = _square(atof(a));
-            printf("%lf", r);
-        }
-        else {
-            printf("Erreur de paramètres");
-        }
-    }
-    else {
-        printf("Erreur de paramètres");
-    }
-    return 0;
+int main(int argc, char *argv[]){
+	if (argc == 4)
+	{
+	char* op = argv[1];
+	char* a = argv[2];
+	char* b = argv[3];
+	double r = 0;
+	if (strcmp(op, "add") == 0) {
+		r = _add(atof(a), atof(b));
+		printf("%lf",r);
+	}
+	else if (strcmp(op, "sub") == 0) {
+		r = _sub(atof(a), atof(b));
+		printf("%lf",r);
+	}
+	else if (strcmp(op, "mul") == 0) {
+		r = _mul(atof(a), atof(b));
+		printf("%lf",r);
+	}
+	else if (strcmp(op, "div") == 0) {
+		r = _div(atof(a), atof(b));
+		printf("%lf",r);
+	}
+	else if (strcmp(op, "carre") == 0) {
+		r = _carre(atof(a));
+		printf("%lf",r);
+	}
+	else {printf("Erreur de parametres");}
+	}
+	else {printf("Erreur de parametres");}
+	return 0;
 }
+
+		
